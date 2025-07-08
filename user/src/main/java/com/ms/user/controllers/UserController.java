ee4e6ec342ec32f4b6a2e7f00d5ab60510df6188
Vinícius Souza <vinicius.fernando.souza@gmail.com>
2025-07-08 19:56:31 -0300

    chore: first commit

---
+++ user/src/main/java/com/ms/user/controllers/UserController.java
@@ -0,0 +1,17 @@
+package com.ms.user.controllers;
+
+import com.ms.user.models.UserModel;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @PostMapping("/users")
+    public ResponseEntity<UserModel> save(@RequestBody @Valid UserRecordDto userRecordDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body();
+    }
+}
---
+++ user/src/main/java/com/ms/user/models/UserModel.java
@@ -0,0 +1,47 @@
+package com.ms.user.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "User")
+public class UserModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    private String name;
+    private String email;
+}
